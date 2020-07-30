add4347f66499ffe42faaaea6b2ca978938f2744
Vladyslav <vladyslav.work@gmail.com>
2020-07-30 12:46:31 +0300

    added basic css/globat css, fonts, html temlate changed

--- pages/posts.js
+++ pages/posts.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
+import { MainLayout } from "./../components/MainLayout";
 
 export default function Posts() {
   return (
-    <>
+    <MainLayout>
       <Head>
         <title>Posts Page | Next Course</title>
       </Head>
       <h2>Posts</h2>
-    </>
+    </MainLayout>
   );
 }
