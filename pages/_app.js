7f1dc6cbb95e92dcb74ac563e0f7c4fbc11ccdbf
Vladyslav <vladyslav.work@gmail.com>
2020-07-30 13:01:02 +0300

    sass testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // import NextNprogress from "nextjs-progressbar";
-import "../styles/main.css";
+import "../styles/main.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return (
