e10a5c47a0a69a66a9b84578e3e713979eb9302d
Vladyslav <vladyslav.work@gmail.com>
2020-07-30 13:34:50 +0300

    main logic for posts

--- pages/posts.js
+++ pages/posts.js
@@ -1,13 +1,59 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
-import { MainLayout } from "./../components/MainLayout";
+import { MainLayout } from "../components/MainLayout";
+import Link from "next/link";
 
-export default function Posts() {
+export default function Posts({ posts: serverPosts }) {
+  const [posts, setPosts] = useState(serverPosts);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("http://localhost:4200/posts");
+      const json = await response.json();
+      setPosts(json);
+    }
+
+    if (!serverPosts) {
+      load();
+    }
+  }, []);
+
+  if (!posts) {
+    return (
+      <MainLayout>
+        <p>Loading ...</p>
+      </MainLayout>
+    );
+  }
+
   return (
     <MainLayout>
       <Head>
         <title>Posts Page | Next Course</title>
       </Head>
-      <h2>Posts</h2>
+      <h1>Posts Page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </MainLayout>
   );
 }
+
+Posts.getInitialProps = async ({ req }) => {
+  if (!req) {
+    return { posts: null };
+  }
+
+  const response = await fetch(`${process.env.API_URL}/posts`);
+  const posts = await response.json();
+
+  return {
+    posts,
+  };
+};
